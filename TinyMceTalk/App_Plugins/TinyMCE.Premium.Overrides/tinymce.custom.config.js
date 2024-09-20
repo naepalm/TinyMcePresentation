e686aec2931aa7007b8a1d174dd98c771bf6f4c4
Janae Cram <me@janaecram.com>
2024-09-20 13:14:43 -0700

    Updating enhanced media embed, export, and footnotes demos

--- TinyMceTalk/App_Plugins/TinyMCE.Premium.Overrides/tinymce.custom.config.js
+++ TinyMceTalk/App_Plugins/TinyMCE.Premium.Overrides/tinymce.custom.config.js
@@ -5,15 +5,20 @@
         window.tinymcepremium.Config.custom_user_config = {
             // Add your config here -- can contain javascript after the ":"
             // YEP, ITS A JAVASCRIPT CALLBACK
-            advtemplate_list: () =>
-                fetch("/umbraco/api/AdvancedTemplates/GetTemplates", {
-                    method: 'GET',
-                })
-                .then((response) => response.json())
-                .then((data) => data)
-                .catch((error) => console.info('Failed to get template list\n' + error)),
-
+            advtemplate_list: () => advTemplatePromise,
         };
+
+        let advTemplatePromise = fetch("/umbraco/api/AdvancedTemplates/GetTemplates", {
+            method: 'GET',
+            })
+            .then((response) => response.json())
+            .then((data) => data)
+            .catch((error) => console.info('Failed to get template list\n' + error));
+
+        // I can execute the promise in the console here, but
+        // if I get it from the custom user config it does not appear to be
+        // executing it and I'm not sure what I'm doing wrong
+        console.info("Template List: ", advTemplatePromise);
     }
 
     /**
