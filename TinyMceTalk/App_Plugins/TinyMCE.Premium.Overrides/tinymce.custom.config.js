fa532e07d1560c4b4593e3521255fcec993dea7c
Janae Cram <janae@proworks.com>
2024-09-30 18:30:49 -0700

    Updates to content for the presentation

--- TinyMceTalk/App_Plugins/TinyMCE.Premium.Overrides/tinymce.custom.config.js
+++ TinyMceTalk/App_Plugins/TinyMCE.Premium.Overrides/tinymce.custom.config.js
@@ -3,21 +3,21 @@
     function init($http, umbRequestHelper) {
         const userRequest = {};
         window.tinymcepremium.Config.custom_user_config = {
-            // To update the advanced templates, you have to set the default to null
-            advtemplate_templates: null,
+            //// To update the advanced templates, you have to set the default to null
+            //advtemplate_templates: null,
 
-            // You can get the entire list from an API response response
-            advtemplate_list: () => umbRequestHelper.resourcePromise(
-                    $http.get('/umbraco/api/AdvancedTemplates/GetTemplates'),
-                    'Failed to get template list'
-                ).then((data) => data),
+            //// You can get the entire list from an API response response
+            //advtemplate_list: () => umbRequestHelper.resourcePromise(
+            //        $http.get('/umbraco/api/AdvancedTemplates/GetTemplates'),
+            //        'Failed to get template list'
+            //    ).then((data) => data),
 
-            // You can get a single item from an API response
-            advtemplate_get_template: (id) => umbRequestHelper.resourcePromise(
-                    $http.get('/umbraco/api/AdvancedTemplates/GetTemplate/?id=' + id),
-                    'Failed to get template list'
-                )
-                .then(({ id, title, content }) => ({ id, title, content })),
+            //// You can get a single item from an API response
+            //advtemplate_get_template: (id) => umbRequestHelper.resourcePromise(
+            //        $http.get('/umbraco/api/AdvancedTemplates/GetTemplate/?id=' + id),
+            //        'Failed to get template list'
+            //    )
+            //    .then(({ id, title, content }) => ({ id, title, content })),
         };
     }
 
